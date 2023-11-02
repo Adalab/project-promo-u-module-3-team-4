cc47ef57e1db16c8a3c7299ab86a74b5d13c702c
BravoCarmen <carmenmbravo@gmail.com>
2023-11-02 12:55:19 +0100

    express with mysql

--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@
 const express = require('express');
 // instalarnos el cors
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 //Crear el servidor
 const app = express(); //mi server
 app.use(cors());
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'sql.freedb.tech',
+    user: 'freedb_grupo4',
+    password: 'tvHR5h6e$Rr4C4D',
+    database: 'freedb_SkyReact',
+  });
+  connection.connect();
+  return connection;
+}
 
-const port = 3000;
+const port = 5001;
 app.listen(port, () => {
   console.log(`Ha arrancado el servidor en http://localhost:${port}`);
+});
+app.get('/listproject', async (req, res) => {
+  const conn = await getConnection();
+  const queryproject = 'select * from project';
+
+  const [result] = await conn.query(queryproject);
+  conn.end();
+  res.json({
+    msj: 'Todo muy bien',
+    data: result,
+  });
 });
 
 //API
@@ -21,16 +43,16 @@ app.listen(port, () => {
 //PUT: actualiza
 //DELETE: eliminar
 
-app.get('/api/adalabers', (request, response) => {
-  //BUSCAR LAS ADALABERS EN LA BASES DE DATOS
-  const adalabers = [{ nombre: 'Alba' }, { nombre: 'Barbara' }];
-  response.json(adalabers);
-});
+// app.get('/api/adalabers', (request, response) => {
+//   //BUSCAR LAS ADALABERS EN LA BASES DE DATOS
+//   const adalabers = [{ nombre: 'Alba' }, { nombre: 'Barbara' }];
+//   response.json(adalabers);
+// });
 
-app.get('/api/profes', (request, response) => {
-  response.send('Hola profes');
-});
+// app.get('/api/profes', (request, response) => {
+//   response.send('Hola profes');
+// });
 
 //Servidor estáticos: mostrar información de ficheros que no cambia
-const staticServerPath = './web';
+const staticServerPath = './web/dist/';
 app.use(express.static(staticServerPath));
